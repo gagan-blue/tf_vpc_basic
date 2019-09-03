c18421ad0d4fe5dc9ffec644e9d28dd458649a2e
gagan <gagan.blue@gmail.com>
2019-09-03 17:24:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_vpc" "tfb" {
   cidr_block           = "${var.cidr}"
-  enable_dns_hostnames = ${var.enable_dns_hostnames}"
-  enable_dns_support   = ${var.enable_dns_support}"
+  enable_dns_hostnames = "${var.enable_dns_hostnames}"
+  enable_dns_support   = "${var.enable_dns_support}"
   tags = {
     Name = "${var.name}"
   }
 }
 
 resource "aws_internet_gateway" "tfb" {
-  vpc_id = ${aws_vpc.tfb.id}"
+  vpc_id = "${aws_vpc.tfb.id}"
   tags = {
     Name = "${var.name}-igw"
   }
