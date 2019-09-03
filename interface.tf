8c0f1382dc106a2fda3b1c27cca35c79af6025a6
gagan <gagan.blue@gmail.com>
2019-09-03 17:20:20 +0530

    Update interface.tf

--- interface.tf
+++ interface.tf
@@ -15,20 +15,17 @@ variable "public_subnet" {
 
 variable "enable_dns_hostnames" {
   description = "Should be true if you want to use private DNS within the VPC"
-  default     = true
-  type        = "bool"
+  default     = "true"
 }
 
 variable "enable_dns_support" {
   description = "Should be true if you want to use private DNS within the VPC"
-  default     = true
-  type        = "bool"
+  default     = "true"
 }
 
 variable "map_public_ip_on_launch" {
   description = "Should be false if you do not want to auto-assign public IP on launch"
-  default     = true
-  type        = "bool"
+  default     = "true"
 }
 
 output "public_subnet_id" {
